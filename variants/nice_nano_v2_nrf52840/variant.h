2dac2c15b7e55b7654da5580e45d74146844dec6
Somik <somik123@users.noreply.github.com>
2025-08-13 18:23:04 +0800

    Fixed pinout

--- variants/nice_nano_v2_nrf52840/variant.h
+++ variants/nice_nano_v2_nrf52840/variant.h
@@ -37,8 +37,8 @@ extern "C"
 #endif // __cplusplus
 
 // Number of pins defined in PinDescription array
-#define PINS_COUNT           (23)
-#define NUM_DIGITAL_PINS     (23)
+#define PINS_COUNT           (48)
+#define NUM_DIGITAL_PINS     (48)
 #define NUM_ANALOG_INPUTS    (3)
 #define NUM_ANALOG_OUTPUTS   (0)
 
@@ -49,11 +49,11 @@ extern "C"
 // analogReadVDDHDIV5() // returns uint32_t
 
 // 3V3 Regulator Disable
-#define PIN_EXT_VCC          (22) // EXT_VCC is P0.13 (set low to disable 3v3)
+#define PIN_EXT_VCC          (13) // EXT_VCC is P0.13 (set low to disable 3v3)
 #define EXT_VCC              (PIN_EXT_VCC)
 
 // LEDs
-#define PIN_LED              (17) // USR LED is P0.15
+#define PIN_LED              (15) // USR LED is P0.15
 #define LED_PWR              (PINS_COUNT) // no LED_PWR
 #define PIN_NEOPIXEL         (PINS_COUNT) // no neopixel
 #define NEOPIXEL_NUM         0
@@ -68,46 +68,70 @@ extern "C"
  */
 #define PIN_BUTTON1          (PINS_COUNT) // no button
 
+
+// P identifiers for the digital pins
+#define PIN_002 (2)  // P0.02 (D19, A0)
+#define PIN_006 (6)  // P0.06 (D1, TX)
+#define PIN_008 (8)  // P0.08 (D0, RX)
+#define PIN_009 (9)  // P0.09 (D10)
+#define PIN_010 (10) // P0.10 (D16)
+#define PIN_011 (11) // P0.11 (D7)
+#define PIN_013 (13) // P0.13 (EXT_VCC)
+#define PIN_015 (15) // P0.15 (D17, LED_BUILTIN)
+#define PIN_017 (17) // P0.17 (D2, SDA)
+#define PIN_020 (20) // P0.20 (D3, SCL)
+#define PIN_022 (22) // P0.22 (D4, SCLK)
+#define PIN_024 (24) // P0.24 (D5, MISO)
+#define PIN_029 (29) // P0.29 (D20, A1)
+#define PIN_031 (31) // P0.31 (D21, A2)
+#define PIN_100 (32) // P1.00 (D6, MOSI)
+#define PIN_101 (33) // P1.01 (D11)
+#define PIN_102 (34) // P1.02 (D12)
+#define PIN_104 (36) // P1.04 (D8)
+#define PIN_106 (38) // P1.06 (D9)
+#define PIN_107 (39) // P1.07 (D13)
+#define PIN_111 (43) // P1.11 (D14)
+#define PIN_113 (45) // P1.13 (D15)
+
+
 // Digital PINs
-// NOTE: THIS IS A RENUMBERING OF THE PINS
-// AND DOES NOT CORRESPOND TO THE NICENANO DIAGRAM
 
 // left side (top to bottom)
-#define D0 (1ul) // P0.08 (RX)
-#define D1 (0ul) // P0.06 (TX)
-#define D2 (2ul) // P0.17 (SDA)
-#define D3 (3ul) // P0.20 (SCL)
-#define D4 (4ul) // P0.22 (SCLK)
-#define D5 (5ul) // P0.24 (MISO)
-#define D6 (6ul) // P1.00 (MOSI)
-#define D7 (7ul) // P0.11
-#define D8 (8ul) // P1.04
-#define D9 (9ul) // P1.06
+#define D0 PIN_008 // P0.08 (RX)
+#define D1 PIN_006 // P0.06 (TX)
+#define D2 PIN_017 // P0.17 (SDA)
+#define D3 PIN_020 // P0.20 (SCL)
+#define D4 PIN_022 // P0.22 (SCLK)
+#define D5 PIN_024 // P0.24 (MISO)
+#define D6 PIN_100 // P1.00 (MOSI)
+#define D7 PIN_011 // P0.11
+#define D8 PIN_104 // P1.04
+#define D9 PIN_106 // P1.06
 
 // right side (bottom to top)
-#define D10 (10ul) // P0.09 (A10)
+#define D10 PIN_009 // P0.09 (A10)
 
 //extra center pins (left to right)
-#define D11 (11ul) // P1.01
-#define D12 (12ul) // P1.02
-#define D13 (13ul) // P1.07
+#define D11 PIN_101 // P1.01
+#define D12 PIN_102 // P1.02
+#define D13 PIN_107 // P1.07
 
 // right side (bottom to top)
-#define D14 (14ul) // P1.11
-#define D15 (15ul) // P1.13
-#define D16 (16ul) // P0.10
-#define D17 (17ul) // P0.25 (Internal LED)
-#define D18 (18ul) // P1.15
-#define D19 (19ul) // P0.02 (A0)
-#define D20 (20ul) // P0.29 (A1)
-#define D21 (21ul) // P0.31 (A2)
+#define D14 PIN_111 // P1.11
+#define D15 PIN_113 // P1.13
+#define D16 PIN_010 // P0.10
+#define D17 PIN_015 // P0.15 (Internal LED)
+#define D18 PIN_115 // P1.15
+#define D19 PIN_002 // P0.02 (A0)
+#define D20 PIN_029 // P0.29 (A1)
+#define D21 PIN_031 // P0.31 (A2)
 
 /*
  * Analog pins
  */
-#define PIN_A0               (19) // P0.02 (D19)
-#define PIN_A1               (20) // P0.29 (D20)
-#define PIN_A2               (21) // P0.31 (D21)
+#define PIN_A0 PIN_002 // P0.02 (D19)
+#define PIN_A1 PIN_029 // P0.29 (D20)
+#define PIN_A2 PIN_031 // P0.31 (D21)
 
 static const uint8_t A0  = PIN_A0 ;
 static const uint8_t A1  = PIN_A1 ;
@@ -117,25 +141,25 @@ static const uint8_t A2  = PIN_A2 ;
 /*
  * Other pins
  */
-#define PIN_NFC1             (10) // P0.09 - also D10
-#define PIN_NFC2             (16) // P0.10 - also D16
+#define PIN_NFC1 PIN_009 // P0.09 - also D10
+#define PIN_NFC2 PIN_010 // P0.10 - also D16
 
 /*
  * Serial interfaces
  */
-#define PIN_SERIAL1_RX       (1) // P0.08 - also D0
-#define PIN_SERIAL1_TX       (0) // P0.06 - also D1
+#define PIN_SERIAL1_RX PIN_008 // P0.08 - also D0
+#define PIN_SERIAL1_TX PIN_006 // P0.06 - also D1
 
 /*
  * SPI Interfaces
  */
 #define SPI_INTERFACES_COUNT 1
 
-#define PIN_SPI_MISO         (4) // P0.20 - D4
-#define PIN_SPI_SCK          (5) // P1.13 - D5
-#define PIN_SPI_MOSI         (6) // P0.10 - D6
+#define PIN_SPI_MISO  PIN_024 // P0.24 - D5
+#define PIN_SPI_SCK   PIN_022 // P0.22 - D4
+#define PIN_SPI_MOSI  PIN_100 // P1.00 - D6
 
-static const uint8_t SS   = (7); // P0.11 - D7
+static const uint8_t SS   = PIN_011; // P0.11 - D7
 static const uint8_t MOSI = PIN_SPI_MOSI ;
 static const uint8_t MISO = PIN_SPI_MISO ;
 static const uint8_t SCK  = PIN_SPI_SCK ;
@@ -145,8 +169,8 @@ static const uint8_t SCK  = PIN_SPI_SCK ;
  */
 #define WIRE_INTERFACES_COUNT 1
 
-#define PIN_WIRE_SDA         (2) // P0.17 - D2
-#define PIN_WIRE_SCL         (3) // P0.20 - D3
+#define PIN_WIRE_SDA  PIN_017 // P0.17 - D2
+#define PIN_WIRE_SCL  PIN_020 // P0.20 - D3
 
 static const uint8_t SDA = PIN_WIRE_SDA;
 static const uint8_t SCL = PIN_WIRE_SCL;
